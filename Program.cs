e845eabbca991e714daa150595aa2fd53357e9f3
Josh <>
2024-01-24 14:32:12 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 3)
+        {
+            Console.WriteLine("Usage: replaceStringInFileNames <directoryPath> <oldString> <newString>");
+            return;
+        }
+
+        string directoryPath = args[0];
+        string oldString = args[1];
+        string newString = args[2];
+
+        DirectoryInfo directoryInfo = new DirectoryInfo(directoryPath);
+        FileInfo[] files = directoryInfo.GetFiles("*" + oldString + "*");
+
+        foreach (FileInfo file in files)
+        {
+            string newFileName = file.Name.Replace(oldString, newString);
+            file.MoveTo(Path.Combine(directoryPath, newFileName));
+            Console.WriteLine("File renamed from {0} to {1}", file.Name, newFileName);
+        }
+
+        Console.WriteLine("All matching file names in {0} have been replaced.", directoryPath);
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,10 @@
+<Directory path> <Old string> <New string>
+
+eg: C:\Users\Josh\Desktop\RenameFiles\RenameFiles.exe "C:\Users\Josh\Desktop\York\Independant Research Project (IRP)\Appendices\Appendix B" B C
+
+In the above example, the code cycles through every file within the "Appendix B" folder, and changes evey instance of the letter "B" to the letter "C".
+
+eg:
+
+Old: "Appendix B.png"
+New: "Appendix C.png"
